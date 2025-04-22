983dc13bcfa43511d9f630f1162cadca91484a67
nomase-bricksuk <nomase.bricksuk@gmail.com>
2025-04-22 17:16:03 +0900

    commit

--- index.php
+++ index.php
@@ -2,13 +2,7 @@
 get_header();
 ?>
 
-<h3>Personal SEO Docs</h3>
 <p>Home Page</p>
-<p>Please Install Advanced Custom Fields Plugin and create</p>
-<ol>
-    <li>1. title (text)</li>
-    <li>2. description (textarea)</li>
-</ol>
 
 <?php
 get_footer();
